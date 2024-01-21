7e24ec2c48e6d62889ab20d7b5330f6f435c9b67
kaviya96 <curlkaviya@gmail.com>
2024-01-21 20:53:21 +1100

    Added test method

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -5,10 +5,20 @@ from todo.todo import ToDo
 def todo_instance():
     return ToDo()  
 
-def test_list_todo(todo_instance, capsys):
+#test to check if the returned value for the method listToDo with option even 
+def test_list_todo_even(todo_instance, capsys):
     todo_instance.listToDo(2, False, "even")
     captured = capsys.readouterr()
     assert "Title:" in captured.out
     assert "Completed:" in captured.out
     assert "quis ut nam facilis et officia qui" in captured.out
     assert "False" in captured.out
+
+
+#test to check if the returned value for the method listToDo with option as odd
+def test_list_todo_odd(todo_instance, capsys):
+    todo_instance.listToDo(10, False, "odd")
+    captured = capsys.readouterr()
+    assert "illo expedita consequatur quia in" in captured.out
+    assert "False" in captured.out
+
