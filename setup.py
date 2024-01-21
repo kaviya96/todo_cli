6575ea7ec664dc3d6f3e13aa81a0bc38a81b45cd
kaviya96 <curlkaviya@gmail.com>
2024-01-22 00:17:57 +1100

    updated test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name = 'todo',
     version = '0.1.0',
     packages = ['todo'],
-    install_requires = ['requests','pytest'], 
+    install_requires = ['requests','pytest', 'unittest'], 
     entry_points = {
         'console_scripts': [
             'todo = todo.__main__:main'
--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,12 +1,23 @@
 import pytest
 from todo.todo import ToDo
+from unittest.mock import patch
 
 @pytest.fixture
 def todo_instance():
     return ToDo()  
+    
+ #test to check the method fetchToDo for a successful request   
+@patch('requests.get')
+def test_fetchToDo_success(mock_get, todo_instance):
+    # Set up the mock response for a successful request
+    mock_response = {'userId': 1, 'id': 1, 'title': 'Test Todo', 'completed': False}
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = mock_response
+    result = todo_instance.fetchToDo(1)
+    assert result == mock_response
 
 #test to check if the returned value for the method listToDo with option even 
-def test_list_todo_even(todo_instance, capsys):
+def test_listtToDo_even(todo_instance, capsys):
     todo_instance.listToDo(2, False, "even")
     captured = capsys.readouterr()
     assert "Title:" in captured.out
@@ -16,7 +27,7 @@ def test_list_todo_even(todo_instance, capsys):
 
 
 #test to check if the returned value for the method listToDo with option as odd
-def test_list_todo_odd(todo_instance, capsys):
+def test_listToDo_odd(todo_instance, capsys):
     todo_instance.listToDo(10, False, "odd")
     captured = capsys.readouterr()
     assert "illo expedita consequatur quia in" in captured.out
